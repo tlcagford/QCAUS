9a508e84407906bdaa48d9e74074d1790154d653
Tony E Ford <tlcagford@gmail.com>
2026-03-28 21:50:56 -0600

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,15 @@
+"""
+QCAUS Core Physics Library
+All physics modules unified under one interface
+"""
+
+from .constants import *
 from .pdp_physics import *
+from .fdm_soliton import *
+from .magnetar_qed import *
+from .primordial_entanglement import *
+from .qcis_power_spectrum import *
+from .stealth_detection import *
+
+__version__ = "1.0.0"
+__author__ = "Tony E. Ford"
